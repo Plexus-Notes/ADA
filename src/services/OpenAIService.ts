790f345af90425d995d828b6f6d76a13e1e20b1f
Pruthviraj Jadhav <pruthvirajjadhav1@gmail.com>
2023-12-30 23:03:44 +0530

    feat: bounty completed

--- src/services/OpenAIService.ts
+++ src/services/OpenAIService.ts
@@ -3,7 +3,7 @@
 import axios from "axios"
 import OpenAI from "openai"
 
-export const API_KEY =
+export const API_KEY = 
   process.env.REACT_APP_OPENAI_API_KEY ?? window.prompt("Whats your openai key?")
 
 //process.env.OPENAI_API_KEY
@@ -20,7 +20,7 @@ async function askGPT4(messages: { role: string; content: string }[]): Promise<s
     const completion = await openai.chat.completions.create({
       //@ts-ignore
       messages: formattedMessages,
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
     })
 
     return completion.choices[0].message.content
@@ -30,4 +30,34 @@ async function askGPT4(messages: { role: string; content: string }[]): Promise<s
   }
 }
 
-export { askGPT4 }
+
+async function shouldITextYet(messages: { role: string; content: string }[]): Promise<string | null> {
+  
+ if (!openai) return null
+  try {
+    const formattedMessages = messages.map((message) => ({
+      role: message.role,
+      content: message.content,
+    }))
+
+    const completion = await openai.chat.completions.create({
+      //@ts-ignore
+      messages: formattedMessages,
+      model: "gpt-3.5-turbo",
+    })
+
+    let content = completion.choices[0].message.content;
+    if (content == "NO" || content == "no" || content == "No" || content == "nO" || content == "n" || content == "N" || content == "No." || content == "NO.") {
+      return null;
+    }
+    return content; 
+  } catch (error) {
+    console.error("Error calling OpenAI API:", error)
+    throw error
+  }
+}
+ 
+  
+
+
+export { askGPT4, shouldITextYet }
